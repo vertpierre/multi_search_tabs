1efd952b61580ab52f8551d86be6aee6a2aa5a25
pierrers-design <roussel.pierre@protonmail.com>
2022-08-16 00:07:06 +0200

    shortcuts solved
    added possibility to use shortcuts and added some comments

--- script.js
+++ script.js
@@ -1,41 +1,93 @@
+// functions to simplify Id selection
+function gId(id) {
+  return document.getElementById(id);
+}
+
 function dosearch() {
-  const sForm = document.searchForm;
-  const sTerms = sForm.searchTerms.value;
+  // regex expression to find elements with "!+word"
+  // and "!+sentence" until next search request
+  // it uses bangs from duckduckgo
   const bang = /!\w+/g;
   const bigBang = /!(.*?)(?=!)|!(.*?)$/g;
 
-  if (bang.test(sTerms)) {
-    const sMultiTerms = sTerms.match(bigBang);
-    for (i = 0; i < sMultiTerms.length; i++) {
-      window.open(sForm.sEngine[1].value + sMultiTerms[i], "_blank");
-    }
+  // naming of elements
+  const sTerms = gId("searchForm").searchTerms;
+  const sEngine = gId("searchForm").searchEngine;
+
+  // detect and allow to use multiple bangs in one line
+  if (bang.test(sTerms.value)) {
+    sTerms.value.match(bigBang).forEach((e) => {
+      window.open(sEngine[1].value + e, "_blank");
+    });
+    // if no bang then use terms with search engines selected
+    // and open multiple tabs if needed
   } else {
-    for (i = 0; i < sForm.sEngine.length; i++) {
-      if (sForm.sEngine[i].checked) {
-        window.open(sForm.sEngine[i].value + sTerms, "_blank");
+    sEngine.forEach((e) => {
+      if (e.checked) {
+        window.open(e.value + sTerms.value, "_blank");
       }
-    }
+    });
   }
-
-  // // console.log(sTerms);
-  // // console.log(bang.test(sTerms));
-
-  // const sMultiTerms = sTerms.match(bigBang);
-  // console.log(sMultiTerms);
 }
 
-// document.onkeydown = function (e) {
-//   e = e || window.event;
-//   k = e.which || e.charCode || e.keyCode;
-//   // Shorcut : Alt + ...
-//   if (k == 71 && e.altKey && !e.ctrlKey && !e.shiftKey) {
-//     // q
-//     document.getElementById("Duckduckgo").checked = true;
-//   } else if (k == 83 && e.altKey && !e.ctrlKey && !e.shiftKey) {
-//     // s
-//     document.getElementById("StartPage").checked = true;
-//   } else {
-//     return true;
-//   }
-//   return false;
-// };
+// var to store first key
+let key1 = {};
+
+// keyboard shortcuts
+window.addEventListener(
+  "keydown",
+  (e) => {
+    if (e.defaultPrevented) {
+      return;
+    }
+
+    // set first key to active
+    key1[e.key] = true;
+    key2 = e.key;
+
+    switch (true) {
+      // first key is stored as active in key1
+      // so a second one can be used at the same time
+      case key1[" "] && key2 == "0":
+        gId("searchForm").searchEngine.forEach((i) => {
+          i.checked = false;
+        });
+        break;
+      case key1[" "] && key2 == "1":
+        gId("searchForm").searchEngine.forEach((i) => {
+          i.checked = true;
+        });
+        break;
+      case key1["d"] && key2 == "g":
+        if (gId("Duckduckgo").checked) {
+          gId("Duckduckgo").checked = false;
+        } else {
+          gId("Duckduckgo").checked = true;
+        }
+        break;
+      case key1["q"] && key2 == "w":
+        if (gId("Qwant").checked) {
+          gId("Qwant").checked = false;
+        } else {
+          gId("Qwant").checked = true;
+        }
+        break;
+      case key1["s"] && key2 == "p":
+        if (gId("StartPage").checked) {
+          gId("StartPage").checked = false;
+        } else {
+          gId("StartPage").checked = true;
+        }
+        break;
+      default:
+        return;
+    }
+    e.preventDefault();
+  },
+  true
+);
+
+// on release the first key stored disabled
+document.addEventListener("keyup", (e) => {
+  key1[e.key] = false;
+});
